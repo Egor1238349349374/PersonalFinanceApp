cfb2964a21679d30aad4f6f61e07be7ae60ec5e9
KEYF <khanakin.ilya10@mail.ru>
2023-06-29 23:12:44 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,222 @@
+﻿
+namespace PersonalFinanceApp
+{
+    partial class Form1
+    {
+
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Label balanceLabel;
+        private System.Windows.Forms.TextBox usernameTextBox;
+        private System.Windows.Forms.Button registerButton;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.TextBox expenseCategoryTextBox;
+        private System.Windows.Forms.TextBox expenseAmountTextBox;
+        private System.Windows.Forms.Button addExpenseButton;
+        private System.Windows.Forms.ListBox expensesListBox;
+        private System.Windows.Forms.TextBox incomeCategoryTextBox;
+        private System.Windows.Forms.TextBox incomeAmountTextBox;
+        private System.Windows.Forms.Button addIncomeButton;
+        private System.Windows.Forms.ListBox incomesListBox;
+
+        private void InitializeComponent()
+        {
+            this.balanceLabel = new System.Windows.Forms.Label();
+            this.usernameTextBox = new System.Windows.Forms.TextBox();
+            this.registerButton = new System.Windows.Forms.Button();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.expenseCategoryTextBox = new System.Windows.Forms.TextBox();
+            this.expenseAmountTextBox = new System.Windows.Forms.TextBox();
+            this.addExpenseButton = new System.Windows.Forms.Button();
+            this.expensesListBox = new System.Windows.Forms.ListBox();
+            this.incomeCategoryTextBox = new System.Windows.Forms.TextBox();
+            this.incomeAmountTextBox = new System.Windows.Forms.TextBox();
+            this.addIncomeButton = new System.Windows.Forms.Button();
+            this.incomesListBox = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // balanceLabel
+            // 
+            this.balanceLabel.AutoSize = true;
+            this.balanceLabel.Location = new System.Drawing.Point(194, 21);
+            this.balanceLabel.Name = "balanceLabel";
+            this.balanceLabel.Size = new System.Drawing.Size(77, 13);
+            this.balanceLabel.TabIndex = 0;
+            this.balanceLabel.Text = "Баланс: $0.00";
+            // 
+            // usernameTextBox
+            // 
+            this.usernameTextBox.Location = new System.Drawing.Point(12, 37);
+            this.usernameTextBox.Name = "usernameTextBox";
+            this.usernameTextBox.Size = new System.Drawing.Size(100, 20);
+            this.usernameTextBox.TabIndex = 1;
+            // 
+            // registerButton
+            // 
+            this.registerButton.Location = new System.Drawing.Point(12, 63);
+            this.registerButton.Name = "registerButton";
+            this.registerButton.Size = new System.Drawing.Size(91, 23);
+            this.registerButton.TabIndex = 2;
+            this.registerButton.Text = "Регистрация";
+            this.registerButton.UseVisualStyleBackColor = true;
+            this.registerButton.Click += new System.EventHandler(this.RegisterButton_Click);
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(109, 63);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 23);
+            this.loginButton.TabIndex = 3;
+            this.loginButton.Text = "Вход";
+            this.loginButton.UseVisualStyleBackColor = true;
+            this.loginButton.Click += new System.EventHandler(this.LoginButton_Click);
+            // 
+            // expenseCategoryTextBox
+            // 
+            this.expenseCategoryTextBox.Location = new System.Drawing.Point(11, 133);
+            this.expenseCategoryTextBox.Name = "expenseCategoryTextBox";
+            this.expenseCategoryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.expenseCategoryTextBox.TabIndex = 4;
+            // 
+            // expenseAmountTextBox
+            // 
+            this.expenseAmountTextBox.Location = new System.Drawing.Point(117, 133);
+            this.expenseAmountTextBox.Name = "expenseAmountTextBox";
+            this.expenseAmountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.expenseAmountTextBox.TabIndex = 5;
+            // 
+            // addExpenseButton
+            // 
+            this.addExpenseButton.Location = new System.Drawing.Point(223, 130);
+            this.addExpenseButton.Name = "addExpenseButton";
+            this.addExpenseButton.Size = new System.Drawing.Size(75, 23);
+            this.addExpenseButton.TabIndex = 6;
+            this.addExpenseButton.Text = "Добавить расход";
+            this.addExpenseButton.UseVisualStyleBackColor = true;
+            this.addExpenseButton.Click += new System.EventHandler(this.AddExpenseButton_Click);
+            // 
+            // expensesListBox
+            // 
+            this.expensesListBox.FormattingEnabled = true;
+            this.expensesListBox.Location = new System.Drawing.Point(11, 159);
+            this.expensesListBox.Name = "expensesListBox";
+            this.expensesListBox.Size = new System.Drawing.Size(287, 95);
+            this.expensesListBox.TabIndex = 7;
+            // 
+            // incomeCategoryTextBox
+            // 
+            this.incomeCategoryTextBox.Location = new System.Drawing.Point(12, 298);
+            this.incomeCategoryTextBox.Name = "incomeCategoryTextBox";
+            this.incomeCategoryTextBox.Size = new System.Drawing.Size(100, 20);
+            this.incomeCategoryTextBox.TabIndex = 8;
+            // 
+            // incomeAmountTextBox
+            // 
+            this.incomeAmountTextBox.Location = new System.Drawing.Point(118, 298);
+            this.incomeAmountTextBox.Name = "incomeAmountTextBox";
+            this.incomeAmountTextBox.Size = new System.Drawing.Size(100, 20);
+            this.incomeAmountTextBox.TabIndex = 9;
+            // 
+            // addIncomeButton
+            // 
+            this.addIncomeButton.Location = new System.Drawing.Point(224, 295);
+            this.addIncomeButton.Name = "addIncomeButton";
+            this.addIncomeButton.Size = new System.Drawing.Size(75, 23);
+            this.addIncomeButton.TabIndex = 10;
+            this.addIncomeButton.Text = "Добавить доход";
+            this.addIncomeButton.UseVisualStyleBackColor = true;
+            this.addIncomeButton.Click += new System.EventHandler(this.AddIncomeButton_Click);
+            // 
+            // incomesListBox
+            // 
+            this.incomesListBox.FormattingEnabled = true;
+            this.incomesListBox.Location = new System.Drawing.Point(12, 324);
+            this.incomesListBox.Name = "incomesListBox";
+            this.incomesListBox.Size = new System.Drawing.Size(287, 95);
+            this.incomesListBox.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 114);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Тип Расхода";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 282);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(67, 13);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Тип Дохода";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(127, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Сумма";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(128, 282);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(41, 13);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Сумма";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 21);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(103, 13);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Имя пользователя";
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(318, 451);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.incomesListBox);
+            this.Controls.Add(this.addIncomeButton);
+            this.Controls.Add(this.incomeAmountTextBox);
+            this.Controls.Add(this.incomeCategoryTextBox);
+            this.Controls.Add(this.expensesListBox);
+            this.Controls.Add(this.addExpenseButton);
+            this.Controls.Add(this.expenseAmountTextBox);
+            this.Controls.Add(this.expenseCategoryTextBox);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.registerButton);
+            this.Controls.Add(this.usernameTextBox);
+            this.Controls.Add(this.balanceLabel);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Личные финансы";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PersonalFinanceApp
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, decimal> accounts;
+        private string currentAccount;
+        private List<Expense> expenses;
+        private List<Income> incomes;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            accounts = new Dictionary<string, decimal>();
+            expenses = new List<Expense>();
+            incomes = new List<Income>();
+
+            // Добавляем обработчик события FormClosing
+            FormClosing += Form1_FormClosing;
+
+            LoadAccountsData();
+            LoadUserData();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Сохраняем данные перед закрытием формы
+            SaveAccountsData();
+            SaveUserData();
+        }
+
+        private void UpdateBalanceLabel()
+        {
+            if (accounts.ContainsKey(currentAccount))
+            {
+                balanceLabel.Text = $"Баланс: {accounts[currentAccount].ToString("C")}";
+            }
+            else
+            {
+                balanceLabel.Text = "Баланс: N/A";
+            }
+        }
+
+        private void UpdateExpensesListBox()
+        {
+            expensesListBox.Items.Clear();
+            List<Expense> accountExpenses = GetExpensesForCurrentAccount();
+
+            foreach (Expense expense in accountExpenses)
+            {
+                expensesListBox.Items.Add(expense);
+            }
+        }
+
+        private void UpdateIncomesListBox()
+        {
+            incomesListBox.Items.Clear();
+            List<Income> accountIncomes = GetIncomesForCurrentAccount();
+
+            foreach (Income income in accountIncomes)
+            {
+                incomesListBox.Items.Add(income);
+            }
+        }
+
+        private List<Expense> GetExpensesForCurrentAccount()
+        {
+            return expenses.Where(expense => expense.Account == currentAccount).ToList();
+        }
+
+        private List<Income> GetIncomesForCurrentAccount()
+        {
+            return incomes.Where(income => income.Account == currentAccount).ToList();
+        }
+
+        private void RegisterButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                MessageBox.Show("Введите имя пользователя.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (accounts.ContainsKey(username))
+            {
+                MessageBox.Show("Пользователь с таким именем уже существует.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            accounts.Add(username, 0);
+            currentAccount = username;
+            UpdateBalanceLabel();
+            ClearUserData();
+
+            SaveAccountsData();
+            SaveUserData();
+        }
+
+        private void LoginButton_Click(object sender, EventArgs e)
+        {
+            string username = usernameTextBox.Text;
+            if (string.IsNullOrWhiteSpace(username))
+            {
+                MessageBox.Show("Введите имя пользователя.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (!accounts.ContainsKey(username))
+            {
+                MessageBox.Show("Пользователь с таким именем не существует.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            currentAccount = username;
+            UpdateBalanceLabel();
+            ClearUserData();
+            ClearExpenseData();
+            ClearIncomeData();
+            LoadUserData();
+        }
+
+
+        private void AddExpenseButton_Click(object sender, EventArgs e)
+        {
+            string category = expenseCategoryTextBox.Text;
+            if (string.IsNullOrWhiteSpace(category))
+            {
+                MessageBox.Show("Введите категорию расхода.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            decimal amount;
+            if (!decimal.TryParse(expenseAmountTextBox.Text, out amount) || amount <= 0)
+            {
+                MessageBox.Show("Введите корректную сумму расхода.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Expense expense = new Expense(category, amount, currentAccount);
+            expenses.Add(expense);
+            accounts[currentAccount] -= amount;
+
+            UpdateBalanceLabel();
+            UpdateExpensesListBox();
+            ClearExpenseData();
+
+            SaveUserData();
+        }
+
+        private void AddIncomeButton_Click(object sender, EventArgs e)
+        {
+            string category = incomeCategoryTextBox.Text;
+            if (string.IsNullOrWhiteSpace(category))
+            {
+                MessageBox.Show("Введите категорию дохода.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            decimal amount;
+            if (!decimal.TryParse(incomeAmountTextBox.Text, out amount) || amount <= 0)
+            {
+                MessageBox.Show("Введите корректную сумму дохода.", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            Income income = new Income(category, amount, currentAccount);
+            incomes.Add(income);
+            accounts[currentAccount] += amount;
+
+            UpdateBalanceLabel();
+            UpdateIncomesListBox();
+            ClearIncomeData();
+
+            SaveUserData();
+            SaveAccountsData();
+        }
+
+        private void LoadAccountsData()
+        {
+            if (File.Exists("accounts.txt"))
+            {
+                using (StreamReader reader = new StreamReader("accounts.txt"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string[] accountData = reader.ReadLine().Split(';');
+                        string username = accountData[0];
+                        decimal balance = decimal.Parse(accountData[1]);
+                        accounts.Add(username, balance);
+                    }
+                }
+            }
+        }
+
+        private void SaveAccountsData()
+        {
+            using (StreamWriter writer = new StreamWriter("accounts.txt"))
+            {
+                foreach (var account in accounts)
+                {
+                    writer.WriteLine($"{account.Key};{account.Value}");
+                }
+            }
+        }
+
+        private void LoadUserData()
+        {
+            if (File.Exists($"{currentAccount}.txt"))
+            {
+                using (StreamReader reader = new StreamReader($"{currentAccount}.txt"))
+                {
+                    while (!reader.EndOfStream)
+                    {
+                        string[] expenseData = reader.ReadLine().Split(';');
+                        string category = expenseData[0];
+                        decimal amount = decimal.Parse(expenseData[1]);
+                        string type = expenseData[2];
+
+                        if (type == "Expense")
+                        {
+                            Expense expense = new Expense(category, amount, currentAccount);
+                            expenses.Add(expense);
+                        }
+                        else if (type == "Income")
+                        {
+                            Income income = new Income(category, amount, currentAccount);
+                            incomes.Add(income);
+                        }
+                    }
+                }
+            }
+
+            UpdateExpensesListBox();
+            UpdateIncomesListBox();
+        }
+
+
+        private void SaveUserData()
+        {
+            using (StreamWriter writer = new StreamWriter($"{currentAccount}.txt"))
+            {
+                List<Expense> userExpenses = GetExpensesForCurrentAccount();
+                List<Income> userIncomes = GetIncomesForCurrentAccount();
+
+                foreach (Expense expense in userExpenses)
+                {
+                    writer.WriteLine($"{expense.Category};{expense.Amount};Expense");
+                }
+
+                foreach (Income income in userIncomes)
+                {
+                    writer.WriteLine($"{income.Category};{income.Amount};Income");
+                }
+            }
+        }
+
+        private void ClearUserData()
+        {
+            expenses.RemoveAll(expense => expense.Account == currentAccount);
+            incomes.RemoveAll(income => income.Account == currentAccount);
+            expensesListBox.Items.Clear();
+            incomesListBox.Items.Clear();
+        }
+
+        private void ClearExpenseData()
+        {
+            expenseCategoryTextBox.Text = "";
+            expenseAmountTextBox.Text = "";
+        }
+
+        private void ClearIncomeData()
+        {
+            incomeCategoryTextBox.Text = "";
+            incomeAmountTextBox.Text = "";
+        }
+    }
+
+    public class Expense
+    {
+        public string Category { get; }
+        public decimal Amount { get; }
+        public string Account { get; }
+
+        public Expense(string category, decimal amount, string account)
+        {
+            Category = category;
+            Amount = amount;
+            Account = account;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Account}] Расход: {Category} - {Amount.ToString("C")}";
+        }
+    }
+
+    public class Income
+    {
+        public string Category { get; }
+        public decimal Amount { get; }
+        public string Account { get; }
+
+        public Income(string category, decimal amount, string account)
+        {
+            Category = category;
+            Amount = amount;
+            Account = account;
+        }
+
+        public override string ToString()
+        {
+            return $"[{Account}] Доход: {Category} + {Amount.ToString("C")}";
+        }
+
+      
+    }
+}
